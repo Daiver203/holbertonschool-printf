aa2db3e51f38fdef3a81dcf2dbaecb8a80c06e80
Daiver203 <daivercontrerasplata@gmail.com>
2022-07-25 10:54:40 -0500

    task

--- format_d.c
+++ format_d.c
@@ -12,7 +12,7 @@ void format_d(va_list arg_add, char *buffer, int *count)
 	int i, len = *count, j = 0;
 	char str[200];
 
-	i = va_artg(arg_add, int);
+	i = va_arg(arg_add, int);
 	itos(str, i);
 	while (str[j] =! '\0')
 	{
