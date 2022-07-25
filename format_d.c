b74faff6d61e716fae7eee688c89ecf2fc8138d7
Daiver203 <daivercontrerasplata@gmail.com>
2022-07-25 11:02:10 -0500

    task

---
+++ format_d.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * format_d - function that returns an int to signed decimal
+ * @valist: arguments passed
+ * @buffer: values stored
+ * @index: tracks index position
+ * @arg_add: data of input
+ * @copy: space of storage
+ * @count: count of int
+*/
+
+void format_d(va_list arg_add, char *buffer, int *count)
+{
+
+}
